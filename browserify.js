1a5d91ee046c5a524c7ed80781addc3f4961103a
Monteiro Steed <steed.monteiro@gmail.com>
2016-04-29 20:37:35 +0200

    manage external events

--- browserify.js
+++ browserify.js
@@ -13,8 +13,9 @@ var Immutable = require('immutable');
 var browserifyUnpack;
 
 function BrowserifyDevTools(options) {
-		this.src = path.resolve(options.src);
-		this.cmd = options.cmd;
+	this.src = path.resolve(options.src);
+	this.cmd = options.cmd;
+	this.stream = null;
 }
 
 BrowserifyDevTools.prototype.init = function(devtoolsLive) {
@@ -30,28 +31,87 @@ BrowserifyDevTools.prototype.init = function(devtoolsLive) {
   		write : process.fs.writeFileSync.bind(process.fs)
     });
 
+	this.file = {
+		path : this.src,
+		src : this.src,
+		url : browserifyUnpack.loaderUrl,
+		deps : []
+	};
+
 	this.devtoolsLive = devtoolsLive;
-	var map = browserifyUnpack.unpack();
-    this.loadMap(map);
+
+	return this.load();
+
 };
 
 BrowserifyDevTools.prototype.loadMap = function(map) {
 	process.live['browserify'] = "";
 	var today = new Date().getTime();
+	var html = [];
+	var urls = [];
+	map.push(this.file);
 	for (var i in map) {
 		map[i].plugin = this;
 		map[i].output = this.output +'/' + map[i].url;
-		this.devtoolsLive.registerFile(map[i]);
-		process.live['browserify'] += "\n<script type='text/javascript' src='/"+map[i].url+"?"+today+"'></script>";
+		var line =  "<script type='text/javascript' src='/"+map[i].url+"?"+today+"'></script>\n";
+
+		process.live['browserify'] += line;
+
+		if(this.devtoolsLive.registerFile(map[i]) == false){
+			html.push({
+				attributes : {
+					src : '/'+map[i].url+"?"+today,
+					type : 'text/javascript'
+				},
+				tag : 'script'
+			});
+			urls.push(map[i].url);
+		}
 	}
 
-	process.live['browserify'] += "\n<script type='text/javascript' src='/loader.js?"+today+"'></script>";
+	return { html : html, urls: urls };
 };
+
+BrowserifyDevTools.prototype.load = function(update) {
+	var  browserifyDevToolsTmpFile = new BrowserifyDevToolsFile(this.devtoolsLive, this.file);
+	var stream = browserifyDevToolsTmpFile.createWriteStream(this.saveLoader.bind(this), update);
+	this.cmd(this.file.src, stream, this.devtoolsLive.onError, []);
+	return stream;
+};
+
+BrowserifyDevTools.prototype.saveLoader = function (browserifyContent, update) {
+
+	var browserifyOutput = browserifyUnpack.unpack(browserifyContent);
+	var data = this.loadMap(browserifyOutput.map);
+
+	this.file.sync = browserifyOutput.loaderContent;
+
+	if(update !== undefined){
+		var record = {
+			action: 'urls',
+			data : data,
+			next : {
+				url :  this.file.url,
+				update : update
+			}
+		};
+
+		this.devtoolsLive.broadcast(record);
+	}
+
+	if(this.stream !== undefined){
+				console.log('js finished');
+
+		this.devtoolsLive.streamFinished(this);
+	}
+};
+
 
 BrowserifyDevTools.prototype.resolve = function(devtoolsLive, file) {
 	var  browserifyDevToolsTmpFile = new BrowserifyDevToolsFile(devtoolsLive, file);
 	this.cmd(file.path, browserifyDevToolsTmpFile.createWriteStream(), devtoolsLive.onError, file.externals);
 };
+
 
 function BrowserifyDevToolsFile( devtoolsLive, file){
 	this.file = file;
@@ -62,13 +122,13 @@ BrowserifyDevToolsFile.prototype.saveFile = function (browserifyContent) {
 
 		var record = {
 			action: 'update',
-			url: this.devtoolsLive.getClientPageUrl() + this.file.url
+			url: this.file.url
 		};
 
 		if (this.file.content === undefined) {
-			record.sync = this.devtoolsLive.getClientHostname() + '/' + this.file.src;
+			record.sync = this.file.src;
 		} else {
-			record.resourceName = this.devtoolsLive.getClientHostname() + '/' + this.file.src;
+			record.resourceName = this.file.src;
 			delete this.file.content;
 		}
 
@@ -79,7 +139,7 @@ BrowserifyDevToolsFile.prototype.saveFile = function (browserifyContent) {
 		if(browserifyFile !== undefined){
 			var fileContent = this.file.line +
 				'\n' + browserifyFile.content + '\n' +
-			'}'+ '\n'+ browserifyFile.mapInline;
+			'}'+'\n'+ browserifyFile.mapInline ;
 
 			this.file.deps.map(function(value, index){
 				if(browserifyFile.info.deps[index] !== undefined){
@@ -87,32 +147,42 @@ BrowserifyDevToolsFile.prototype.saveFile = function (browserifyContent) {
 				}
 			});
 
-			console.log('file', this.file.deps);
-			console.log('browserifyFile', browserifyFile.info.deps);
-
 			var result = Immutable.Map(browserifyFile.info.deps);
 			var equals = Immutable.is(this.file.deps, result);
 
 			this.file.deps = result;
 
 			record.content = fileContent ;
-			this.devtoolsLive.broadcast(record);
+			record.deps = equals;
 
 			process.fs.writeFileSync(this.file.output, record.content);
+
+			if(equals == false){
+				this.file.plugin.load(record);
+			}else{
+				this.devtoolsLive.broadcast(record);
+			}
 
 		}
 
 };
 
 
-BrowserifyDevToolsFile.prototype.createWriteStream  = function() {
+BrowserifyDevToolsFile.prototype.createWriteStream  = function(next,record) {
 
     var data = []; // We'll store all the data inside this array
     var writeStream = function (chunk) {
       data.push(chunk);
     };
-    var endStream  = function() { // Will be emitted when the input stream has ended, ie. no more data will be provided
-      this.saveFile(Buffer.concat(data).toString());
+    var endStream  = function() {
+    	var content = Buffer.concat(data).toString();
+    	if(next !== undefined){
+    		return next(content, record);
+    	}else{
+    		return this.saveFile(content);
+    	}
+	    // Will be emitted when the input stream has ended, ie. no more data will be provided
+
     }.bind(this);
 
     return ES.through(writeStream, endStream);
